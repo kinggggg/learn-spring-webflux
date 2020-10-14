52beadd8c7b7e23c2c0c564bd4e8b79cd674cf1f
kingggg <liweibooooo@126.com>
2020-10-15 00:39:53 +0800

    aop监控方法调用

--- src/main/java/com/zeek/learnspringwebflux/aop/ApiCallAspect.java
+++ src/main/java/com/zeek/learnspringwebflux/aop/ApiCallAspect.java
@@ -4,6 +4,7 @@ import com.zeek.learnspringwebflux.exception.BusinessException;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -28,8 +29,12 @@ public class ApiCallAspect {
     public void doAround(ProceedingJoinPoint proceedingJoinPoint) {
 
         try {
+            System.out.println("service方法执行前");
+
             Object proceed = proceedingJoinPoint.proceed();
             System.out.println(proceed);
+
+            System.out.println("service方法执行后");
         } catch (Throwable throwable) {
             System.out.println("error: 系统错误");
             throw new BusinessException(500, "系统错误");
