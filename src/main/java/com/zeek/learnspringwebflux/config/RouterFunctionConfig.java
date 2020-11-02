4d9b89418f548a201764b8546efb804dfbb12d9b
liweibo03 <liweibo03@kuaishou.com>
2020-11-02 21:05:09 +0800

    函数式controller

--- src/main/java/com/zeek/learnspringwebflux/config/RouterFunctionConfig.java
+++ src/main/java/com/zeek/learnspringwebflux/config/RouterFunctionConfig.java
@@ -1,13 +1,20 @@
 package com.zeek.learnspringwebflux.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.server.RouterFunction;
-
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 import static reactor.core.publisher.Flux.just;
+
+import java.net.URI;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.zeek.learnspringwebflux.domain.User;
+
+import reactor.core.publisher.Mono;
 
 /**
  * @ClassName RouterFunctionConfig
@@ -22,9 +29,17 @@ public class RouterFunctionConfig {
     @Bean
     public RouterFunction<?> helloRouterFunction() {
 
+        User user = new User();
+        user.setAge(18);
+        user.setGender("man");
+        user.setName("zhangsan");
+        Mono<User> userMono = Mono.just(user);
+
         return route(GET("/hello"),
-                serverRequest -> ok().body(just("Hello World"), String.class))
+                        serverRequest -> ok().body(just("Hello World"), String.class))
                .andRoute(GET("/bye"),
-                serverRequest -> ok().body(just("See ya!"), String.class));
+                        serverRequest -> ok().body(just("See ya!"), String.class))
+               .andRoute(GET("/test"),
+                        serverRequest -> ServerResponse.created(URI.create("http://localhost:8081/testtest")).body(userMono, User.class));
     }
 }
